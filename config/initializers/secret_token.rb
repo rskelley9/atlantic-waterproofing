1a3471f4889b2f34157e407cd232defec3c09473
Ryan Kelley <rskelley9@gmail.com>
2014-06-30 22:35:45 -0400

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AltaJuly::Application.config.secret_key_base = 'aba1cb31ab97e5048ab8114b7b918ba9bd19699d8d6fb2119fb7fc861037b70d235e2b0d0daa519dbda3e81e6461f0e1cb0ccc81ffd06d0d2ddbf3adcbb76dac'

b0e63359be0aeef977947e5b898525b8196f96bc
Ryan Kelley <rskelley9@gmail.com>
2014-08-06 15:09:58 -0400

    Improved email form with first and last name, improving client side validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,9 +4,9 @@ class Message
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :name, :email, :subject, :body
+  attr_accessor :first_name, :last_name, :email, :subject, :body
 
-  validates :name, :email, :subject, :body, :presence => true
+  validates :first_name, :last_name, :email, :subject, :body, :presence => true
   validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => false
 
   def initialize(attributes = {})

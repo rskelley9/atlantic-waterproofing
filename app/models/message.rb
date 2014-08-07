5d753cb625c58bca531a1fd19d6124377037dc73
Ryan Kelley <rskelley9@gmail.com>
2014-08-07 02:00:28 -0400

    pretty errors

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,9 +6,10 @@ class Message
 
   attr_accessor :first_name, :last_name, :email, :subject, :body
 
-  validates :first_name, :last_name, :email, :email_confirmation, :subject, :body, :presence => true
-  validates :email, format: { with: %r{.+@.+\..+}, :allow_blank => false,  message: "%{value} is not a valid email address." }
-  validates :email, confirmation: true
+  validates :first_name, :last_name, :email, :email_confirmation, :subject, :body, presence: true
+  validates :email, format: { with: %r{.+@.+\..+}, allow_blank: false,  message: "%{value} is not a valid email address." }
+  validates :email, confirmation: { message: "The email entries you provided do not match. Please fix them and try again." }
+  validates :body, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 
   def initialize(attributes = {})
     attributes.each do |name, value|

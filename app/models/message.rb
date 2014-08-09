149ec31259cddb72f2f3f6d94b412c6e2343ead7
Ryan Kelley <rskelley9@gmail.com>
2014-08-09 02:19:23 -0400

    corrected error typo

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,7 +8,7 @@ class Message
 
   validates :first_name, :last_name, :email, :email_confirmation, :subject, :body, presence: true
   validates :email, format: { with: %r{.+@.+\..+}, allow_blank: false,  message: "%{value} is not a valid email address." }
-  validates :email, confirmation: { message: "%{value} does not match the email addres you provided. Please fix them and try again." }
+  validates :email, confirmation: { message: "%{value} does not match the email address you provided. Please correct the error and submit the form again." }
   validates :body, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
   validates :phone, :presence => true, :numericality => true, length: { :minimum => 10, :maximum => 15, too_short: "is too short (minimum is 10 numerical characters)." }
 

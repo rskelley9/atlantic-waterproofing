9fa132acc81ac0df643191b93fc7fd83e85e46c3
Ryan Kelley <rskelley9@gmail.com>
2014-08-19 03:53:17 -0400

    .

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -91,8 +91,8 @@ end
 
 # Default credentials for ComfortableMexicanSofa::HttpAuth
 # YOU REALLY WANT TO CHANGE THIS BEFORE PUTTING YOUR SITE LIVE
-ComfortableMexicanSofa::HttpAuth.username = 'username'
-ComfortableMexicanSofa::HttpAuth.password = 'password'
+ComfortableMexicanSofa::HttpAuth.username = ENV["CMS_USERNAME_PRO"]
+ComfortableMexicanSofa::HttpAuth.password = ENV["CMS_PASSWORD_PRO"]
 
 # You can use bcrypt (gem 'bcrypt-ruby') if you want to:
 #   require 'bcrypt'
